a9cf876c0a760f4c255e2fa4366872d4ea4f0286
Anton Surkis <antonsurkis@gmail.com>
2023-04-29 17:17:02 +0300

    Solve task M

--- src/bin/task_m.rs
+++ src/bin/task_m.rs
@@ -19,17 +19,28 @@ fn main() {
 fn solve(_m: usize, cubes: &[usize]) -> Vec<usize> {
     let n = cubes.len();
     let mut max_paly = vec![0; n];
+    let mut c = 0;
+    let mut r = 0;
     for i in 0..n {
-        for j in 0..i {
+        let mut k = 0;
+        if i < r {
+            let j = c + c - i;
+            k = max_paly[j].min(r - i);
+        }
+        for j in k..i {
             if i + j >= n {
                 break;
-            }
-            if cubes[i + j] == cubes[i - j - 1] {
-                max_paly[i] += 1;
+            } else if cubes[i + j] == cubes[i - j - 1] {
+                k += 1;
             } else {
                 break;
             }
         }
+        if i + k > r {
+            c = i;
+            r = i + k;
+        }
+        max_paly[i] = k;
     }
 
     debug!(&max_paly);
@@ -46,6 +57,31 @@ fn solve(_m: usize, cubes: &[usize]) -> Vec<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn _baseline(_m: usize, cubes: &[usize]) -> Vec<usize> {
+        let n = cubes.len();
+        let mut max_paly = vec![0; n];
+        for i in 0..n {
+            for j in 0..i {
+                if i + j >= n {
+                    break;
+                }
+                if cubes[i + j] == cubes[i - j - 1] {
+                    max_paly[i] += 1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        let mut answer = vec![];
+        for i in 0..n {
+            if max_paly[i] == i {
+                answer.push(n - i);
+            }
+        }
+        answer
+    }
 
     #[test]
     fn test1() {
